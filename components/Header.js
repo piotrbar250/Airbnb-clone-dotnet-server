f5d8c0e3560cad1ce44a4bf2ee578752c20ac2af
Piotr Baranowski <piotr.baranowski250@gmail.com>
2023-11-11 04:13:27 +0100

    Created header and banner

---
+++ components/Header.js
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import { Input } from 'postcss'
+import React from 'react'
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+  SearchIcon
+} from '@heroicons/react/solid'
+
+export default function Header() {
+  return (
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
+
+      {/* Left */}
+      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+        <Image
+          src='https://links.papareact.com/qd3'
+          layout='fill'
+          objectFit='contain'
+          objectPosition='left'
+        />
+      </div>
+
+      {/* Middle */}
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <input className='flex-grow pl-5 bg-transparent outline-none' type='text' placeholder='Start your search'/>
+        <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
+      </div>
+
+      {/* Right */}
+      <div className='flex items-center space-x-4 justify-end'>
+        <p className='hidden md:inline md:cursor-pointer'>Become a host</p>
+        <GlobeAltIcon className='h-6 cursor-pointer pr-4'/>
+        <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+          <MenuIcon className='h-6 cursor-pointer'/>
+          <UserCircleIcon className='h-6 cusor-point'/>
+        </div>
+      </div>
+    </header>
+  )
+}
