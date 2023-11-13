c3d907c4eb14e8dd1b099a0696a2ac53eb90df1d
Piotr Baranowski <piotr.baranowski250@gmail.com>
2023-11-13 18:03:03 +0100

    Initial create

--- server/Program.cs
+++ server/Program.cs
@@ -1,16 +1,8 @@
-
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddControllers();
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy("CorsPolicy",
-//         builder => builder.WithOrigins("http://localhost:3000/")
-//                           .AllowAnyHeader()
-//                           .AllowAnyMethod());
-// });
+
 var app = builder.Build();
 app.MapGet("/", () => "Hello there");
-// app.UseCors();
 
 app.MapControllers();
 
